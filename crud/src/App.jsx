320047f6444d2e2c3fc6ff976c3ac06185d8f4cc
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-04 21:27:39 +0300

    initial commit

---
+++ crud/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { Navbar } from './components/Layout'
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <h1>Hello, world!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ crud/src/components/Layout.jsx
@@ -0,0 +1,37 @@
+
+
+export function Navbar(){
+    return (
+        <nav className="navbar navbar-expand-lg bg-white border-bottom box-shadow">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    <img src="./public/images/icon.png" alt="dev" alt='...' width= '30' className="me-2"/>Best Store
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                                <a className="nav-link text-dark" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="#">Contact</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><hr className="dropdown-divider"/></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav> 
+    )
+}
